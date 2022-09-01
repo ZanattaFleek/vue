f6375ac7e19952c867cb206e25c5505b84675337
zanattafleek <zanattafleek@gmail.com>
2022-09-01 15:20:52 -0300

    FormularioInicial

--- formularios/src/cadastros/Colaborador.vue
+++ formularios/src/cadastros/Colaborador.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        Componente Colaborador OK!
+        <label for="txtNome">Nome:</label>
+        <input type="text" id="txtNome" value="" v-model="rsColaborador.nome">
+
+        <label for="txtEndereco">Endereço:</label>
+        <input type="text" id="txtEndereco" value="" v-model="rsColaborador.endereco">
+
+        <div class="debug">
+            {{ rsColaborador }}
+        </div>
+
     </div>
 </template>
 
@@ -8,3 +17,9 @@
 import ColaboradorCls from './ColaboradorCls'
 export default ColaboradorCls
 </script>
+
+<style>
+.debug {
+    margin-top: 100px;
+}
+</style>
--- formularios/src/cadastros/ColaboradorCls.ts
+++ formularios/src/cadastros/ColaboradorCls.ts
@@ -1,6 +1,18 @@
 import { Component, Vue } from 'vue-property-decorator';
 
+interface CadastroColaboradorInterface {
+    nome: string
+    endereco: string
+}
+
 @Component
 export default class ColaboradorCls extends Vue {
+
+    public rsColaborador: CadastroColaboradorInterface = {
+        nome: '',
+        endereco: ''
+    }
+
+
 
 }

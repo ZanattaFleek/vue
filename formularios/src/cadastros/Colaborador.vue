08aa8d625450d1db02d006ee5547aa2f9abfa0ff
zanattafleek <zanattafleek@gmail.com>
2022-09-01 15:13:33 -0300

    Formularios

---
+++ formularios/src/cadastros/Colaborador.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Componente Colaborador OK!
+    </div>
+</template>
+
+<script lang="ts">
+import ColaboradorCls from './ColaboradorCls'
+export default ColaboradorCls
+</script>
